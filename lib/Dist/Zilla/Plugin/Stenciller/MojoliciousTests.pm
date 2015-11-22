0f53e6e9c57436f27cebda69cd49e1eebd3ea2a8
Erik Carlsson <info@code301.com>
2015-11-22 15:06:31 +0000

    Init

---
+++ lib/Dist/Zilla/Plugin/Stenciller/MojoliciousTests.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::Stenciller::MojoliciousTests;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::Stenciller::MojoliciousTests;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::Stenciller::MojoliciousTests is ...
+
+=head1 SEE ALSO
+
+=cut
