e20c410673d47a302cde262cf5f2add480edf6eb
Erik Carlsson <info@code301.com>
2016-03-23 20:16:22 +0000

    use some Type::Tiny libraries (fixes #1). Lower Perl req to 5.10.

--- lib/Dist/Zilla/Plugin/Stenciller/MojoliciousTests.pm
+++ lib/Dist/Zilla/Plugin/Stenciller/MojoliciousTests.pm
@@ -1,82 +1,83 @@
-use 5.14.0;
+use 5.10.0;
 use warnings;
 
-package Dist::Zilla::Plugin::Stenciller::MojoliciousTests {
+package Dist::Zilla::Plugin::Stenciller::MojoliciousTests;
 
-    # VERSION
-    # ABSTRACT: Create Mojolicious tests from text files parsed with Stenciller
+# ABSTRACT: Create Mojolicious tests from text files parsed with Stenciller
+# AUTHORITY
+our $VERSION = '0.0200';
 
-    use Moose;
-    with 'Dist::Zilla::Role::FileGatherer';
-    use Stenciller;
-    use Types::Stenciller -types;
-    use Path::Tiny;
-    use Dist::Zilla::File::InMemory;
-    use namespace::autoclean;
+use Moose;
+with 'Dist::Zilla::Role::FileGatherer';
+use Stenciller;
+use Types::Stenciller -types;
+use Types::Standard qw/Str/;
+use Types::Path::Tiny qw/AbsFile Dir Path/;
+use Path::Tiny;
+use Dist::Zilla::File::InMemory;
+use namespace::autoclean;
 
-    has source_directory => (
-        is => 'ro',
-        isa => Dir,
-        coerce => 1,
-        default => 'examples/source',
-    );
-    has file_pattern => (
-        is => 'ro',
-        isa => Str,
-        default => '.+\.stencil',
-    );
-    has output_directory => (
-        is => 'ro',
-        isa => Path,
-        coerce => 1,
-        default => 't',
-    );
-    has template_file => (
-        is => 'ro',
-        isa => AbsFile,
-        lazy => 1,
-        coerce => 1,
-        default => sub { shift->source_directory->child('template.test')->absolute },
-    );
+has source_directory => (
+    is => 'ro',
+    isa => Dir,
+    coerce => 1,
+    default => 'examples/source',
+);
+has file_pattern => (
+    is => 'ro',
+    isa => Str,
+    default => '.+\.stencil',
+);
+has output_directory => (
+    is => 'ro',
+    isa => Path,
+    coerce => 1,
+    default => 't',
+);
+has template_file => (
+    is => 'ro',
+    isa => AbsFile,
+    lazy => 1,
+    coerce => 1,
+    default => sub { shift->source_directory->child('template.test')->absolute },
+);
 
-    sub gather_files {
-        my $self = shift;
+sub gather_files {
+    my $self = shift;
 
-        my $template = $self->template_file->slurp_utf8;
-        my @source_files = $self->source_directory->children(qr{^@{[ $self->file_pattern ]}$});
+    my $template = $self->template_file->slurp_utf8;
+    my @source_files = $self->source_directory->children(qr{^@{[ $self->file_pattern ]}$});
 
-        $self->log('Generating tests from stencils');
+    $self->log('Generating tests from stencils');
 
-        foreach my $file (@source_files) {
-            my $contents = Stenciller->new(filepath => $file->stringify)->transform(
-                plugin_name => 'ToMojoliciousTest',
-                transform_args => {
-                    require_in_extra => {
-                        key => 'is_test',
-                        value => 1,
-                        default => 1
-                    },
+    foreach my $file (@source_files) {
+        my $contents = Stenciller->new(filepath => $file->stringify)->transform(
+            plugin_name => 'ToMojoliciousTest',
+            transform_args => {
+                require_in_extra => {
+                    key => 'is_test',
+                    value => 1,
+                    default => 1
                 },
-                constructor_args => {
-                    template => $self->template_file,
-                },
-            );
+            },
+            constructor_args => {
+                template => $self->template_file,
+            },
+        );
 
-            my $new_filename = $file->basename(qr/\.[^.]+$/) . '.t';
-            $self->log("Generated $new_filename");
+        my $new_filename = $file->basename(qr/\.[^.]+$/) . '.t';
+        $self->log("Generated $new_filename");
 
-            my $generated_file = Dist::Zilla::File::InMemory->new(
-                name => path($self->output_directory, $new_filename)->stringify,
-                content => $contents,
-            );
-            $self->add_file($generated_file);
+        my $generated_file = Dist::Zilla::File::InMemory->new(
+            name => path($self->output_directory, $new_filename)->stringify,
+            content => $contents,
+        );
+        $self->add_file($generated_file);
 
-        }
     }
-
-    __PACKAGE__->meta->make_immutable;
-
 }
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 
